eab72b5709f505b9c3678e2db0fd4a39cad85d78
Adrian <18417539+adriankarlen@users.noreply.github.com>
2022-02-23 09:50:54 +0100

    Generalized DB config settings

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize('treble', 'root', '', {
-    dialect: 'mysql',
-    host: 'localhost', 
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    dialect: process.env.DB_DIALECT,
+    host: process.env.DB_HOST, 
 });
 
 export default sequelize;
